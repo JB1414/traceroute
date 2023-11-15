aaa3cc5424a99409d43caf3e3ae509d629cf4b0c
Yulechka <JB1404@yandex.ru>
2023-11-16 02:34:49 +0500

    Added argument checks

--- main.py
+++ main.py
@@ -30,37 +30,62 @@ def get_packet(address, length, seq):
         try:
             ipaddress.ip_address(address)
         except Exception:
-            print("Invalid address 1")
+            print("Invalid address")
             sys.exit(-1)
         return ICMPv4Packet(address, length, seq)
     elif re.match(ipv6_pattern, address):
         try:
             ipaddress.IPv6Address(address)
         except ipaddress.AddressValueError:
-            print("Invalid address 2")
+            print("Invalid address")
             sys.exit(-1)
         return ICMPv6Packet(address, length, seq)
     elif re.match(domain_name_pattern, address):
         try:
-            socket.gethostbyname(address)
+            ip_address = socket.gethostbyname(address)
         except socket.gaierror:
-            print("Invalid address 3")
+            print("Invalid address")
             sys.exit(-1)
-        return ICMPv4Packet(address, length, seq)
+        return ICMPv4Packet(ip_address, length, seq)
     else: 
-        print("Invalid address 4")
+        print("Invalid address")
         sys.exit(-1)
 
+def check_length(len):
+    if len < 8:
+        print(f"The packet length can't be {len}")
+        sys.exit(-3)
+
+def check_timeout(timeout):
+    if timeout < 0:
+        print(f"Timeout can't be negative")
+        sys.exit(-2)
+
+def check_interval(interval):
+    if interval < 0:
+        print(f"Interval can't be negative")
+
+def check_repeat(repeat):
+    if repeat < 0:
+        print(f"Repeat can't be negative")
+
+def check_maxhops(maxhops):
+    if maxhops < 0:
+        print(f"Maxhops (TTL) cannot be negative")
+
+def check_arguments(args):
+    check_length(args.length)
+    check_timeout(args.timeout)
+    check_interval(args.interval)
+    check_repeat(args.repeat)
+    check_maxhops(args.maxhops)
 
 def main():
-    '''args = get_arguments()
+    args = get_arguments()
+    check_arguments(args)
     address = str(args.address)
     packet = get_packet(address, args.length, args.seq)
     tracert = Traceroute(packet, args.timeout, args.interval, args.repeat, args.maxhops, args.debugmode)
-    tracert.traceroute()'''
-    address = "8.8.8.8"
-    packet = get_packet(address, 40, 0)
-    tracert = Traceroute(packet, 1, 0, 3, 30)
     tracert.traceroute()
 
 
